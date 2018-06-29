825e471981f901216545e16c65f45525a6df4dd8
achavan <amey.chavan@b2x.com>
2018-06-29 12:25:05 +0530

    concated array

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 let q = require('q')
 
 let qSyncAll = function (functionName, nos, ...args) {
-  // console.log(functionName, nos,...args)
-  // console.log("#################")
+  nos = nos.concat([])
   let p = q()
   let thePromises = []
 
   nos.forEach(function (file) {
     p = p
       .then(function () {
-        return functionName(file,...args)
+        return functionName(file, ...args)
       })
 
       .catch(function (error) {
@@ -43,9 +42,10 @@ let qAsyncAll = function (functionName, nos, ...args) {
     resolve: [],
     reject: []
   }
+  nos = nos.concat([])
 
   return q.all(nos.map(function (obj) {
-    return functionName(obj ,...args).catch(function (error) {
+    return functionName(obj , ...args).catch(function (error) {
       data.reject.push(error)
     })
   }))
@@ -67,6 +67,7 @@ let qAsyncAll = function (functionName, nos, ...args) {
 let qASyncWithBatch = function (functionName, nos, batchSize = 10, ...args) {
   let p = q()
   let thePromises = []
+  nos = nos.concat([])
 
   let arrays = []
   while (nos.length > 0) { arrays.push(nos.splice(0, batchSize)) }
